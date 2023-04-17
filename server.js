16838c58899fb00031339cd691074910d237b38b
beryln-t <berylntee@gmail.com>
2023-04-17 10:25:36 +0800

    Setup

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "dist")));
 
-app.get("/*", function (req, res) {
+app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
