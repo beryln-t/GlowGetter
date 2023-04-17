e2a8bd00fb401db2196cda1b70445d417633db4b
beryln-t <berylntee@gmail.com>
2023-04-17 10:07:30 +0800

    Setup

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const path = require("path");
+const logger = require("morgan");
+require("dotenv").config();
+require("./config/database");
+// const jwt = require("jsonwebtoken");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "dist")));
+
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
