51db3fa5798df0cb5dcff065b1284262d5bf0f51
beryln-t <berylntee@gmail.com>
2023-04-20 23:01:16 +0800

    set up tailwind and daisyui

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from "react";
-
+import NavBar from "./components/NavBar/NavBar";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <div className="App">Hi world</div>;
+  return (
+    <div className="App">
+      <NavBar />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,5 @@
+import { Link, useNavigate, NavLink } from "react-router-dom";
+
+export default function NavBar() {
+  return <button className="btn glass">Glass button</button>;
+}
