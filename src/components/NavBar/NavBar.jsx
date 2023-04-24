345307e8c94b0b99a9c371fdb21d1e0f660fb0c4
beryln-t <berylntee@gmail.com>
2023-04-24 13:43:48 +0800

    Revert "fix navbar button"
    This reverts commit af9883b4e16d56e046ee8c576fc92404f55568c6.

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -7,8 +7,8 @@ export default function NavBar({ user, setUser }) {
   console.log(user);
 
   return (
-    <div className="navbar sticky top-0 z-50 bg-slate-300 px-5">
-      <div className="navbar-start text-slate-700">
+    <div className="navbar sticky top-0 z-50 bg-slate-300">
+      <div className="navbar-start text-slate-700	mr-20">
         <div className="dropdown text-slate-700		">
           <label tabIndex={0} className="btn btn-ghost lg:hidden">
             <svg
@@ -43,7 +43,7 @@ export default function NavBar({ user, setUser }) {
             </li>
           </ul>
         </div>
-        <p className="text text-xl font-black	font-serif">GlowGetter</p>
+        <p className="text text-xl font-black	font-serif ml-20">GlowGetter</p>
       </div>
       <div className="centerMenu navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1 text-slate-700		">
