4acf42cd5e416b8c4c3a8106ffab50a083464939
beryln-t <berylntee@gmail.com>
2023-04-24 11:58:03 +0800

    fix password hashing error

--- models/User.js
+++ models/User.js
@@ -63,5 +63,13 @@ const userSchema = new Schema(
   }
 );
 
+userSchema.pre("save", async function (next) {
+  // 'this' is the user doc
+  if (!this.isModified("password")) return next();
+  // update the password with the computed hash
+  this.password = await bcrypt.hash(this.password, SALT_ROUNDS);
+  return next();
+});
+
 const User = mongoose.model("User", userSchema);
 module.exports = User;
